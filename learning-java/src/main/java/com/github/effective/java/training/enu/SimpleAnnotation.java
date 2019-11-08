21381e4ef47fd6e386cba4c9b63dbcd9a9c22dc5
yanghuadong <314726028@qq.com>
2019-11-08 17:48:32 +0800

    Effective Java枚举与注解章节

---
+++ learning-java/src/main/java/com/github/effective/java/training/enu/SimpleAnnotation.java
@@ -0,0 +1,29 @@
+package com.github.effective.java.training.enu;
+
+/**
+ * @author BirdSnail
+ * @date 2019/11/6
+ */
+public class SimpleAnnotation {
+    @Test
+    public static void m1() {
+
+    }
+
+    public static void m2() {
+
+    }
+
+    @Test
+    public static void m3() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public  void m4() {
+
+    }
+    public static void m5() {
+
+    }
+}
---
+++ learning-java/src/main/java/com/github/effective/java/training/enu/Test.java
@@ -0,0 +1,11 @@
+package com.github.effective.java.training.enu;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {
+}
