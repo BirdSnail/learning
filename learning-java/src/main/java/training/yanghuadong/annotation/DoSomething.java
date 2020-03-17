e0ebf7a889a9c16ca8b268dd228acfcbaded967e
yanghuadong <314726028@qq.com>
2020-03-17 17:30:43 +0800

    feat(annotation):学习了注解

--- learning-java/src/main/java/training/yanghuadong/annotation/DoSomething.java
+++ learning-java/src/main/java/training/yanghuadong/annotation/DoSomething.java
@@ -5,4 +5,24 @@ package training.yanghuadong.annotation;
  * @date 2020/3/16
  */
 public class DoSomething {
+
+    @MyRunTest
+    public void addition() {
+        System.out.println(1 + 1);
+    }
+
+    @MyRunTest
+    public void subtraction() {
+        System.out.println(5 - 1);
+    }
+
+    @MyRunTest
+    public void multiplication() {
+        System.out.println(3 * 5);
+    }
+
+    @MyRunTest
+    public void division() {
+        System.out.println(5 / 0);
+    }
 }
--- learning-java/src/main/java/training/yanghuadong/annotation/MyRunTestAnnotationTool.java
+++ learning-java/src/main/java/training/yanghuadong/annotation/MyRunTestAnnotationTool.java
@@ -1,8 +1,42 @@
 package training.yanghuadong.annotation;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author BirdSnail
  * @date 2020/3/16
  */
 public class MyRunTestAnnotationTool {
+
+    public static void main(String[] args)  {
+        final Class<DoSomething> doSomethingClass = DoSomething.class;
+        final Constructor<?> constructor = doSomethingClass.getConstructors()[0];
+        Object instance = null;
+        try {
+            instance = constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        List<Exception> exceptionList = new ArrayList<>();
+        final Method[] methods = doSomethingClass.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyRunTest.class)) {
+                try {
+                    method.invoke(instance);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    exceptionList.add(e);
+                }
+            }
+        }
+
+        if (!exceptionList.isEmpty()) {
+            System.out.println("程序由错误：" + exceptionList.size());
+            exceptionList.forEach(System.out::println);
+        }
+    }
 }
