916150818724da434c1c68fa6aa97243e5210dc6
yanghuadong <314726028@qq.com>
2020-03-17 17:29:59 +0800

    feat(nio):学习了nio

--- learning-java/src/main/java/training/yanghuadong/nio/BuffTest.java
+++ learning-java/src/main/java/training/yanghuadong/nio/BuffTest.java
@@ -1,8 +1,37 @@
 package training.yanghuadong.nio;
 
+import java.nio.IntBuffer;
+import java.util.Arrays;
+
 /**
  * @author BirdSnail
  * @date 2019/12/4
  */
 public class BuffTest {
+
+    public static void main(String[] args) {
+        // 默认为写模式
+        IntBuffer intBuffer = IntBuffer.allocate(10);
+        intBuffer.get();
+        intBuffer.get();
+        intBuffer.get();
+        intBuffer.get();
+
+        intBuffer.put(1);
+        intBuffer.put(2);
+        intBuffer.put(3);
+//        System.out.println(intBuffer.position());
+
+        intBuffer.flip();
+        System.out.println(intBuffer.get());
+        System.out.println(intBuffer.get());
+        System.out.println(intBuffer.position());
+        // 剩余元素复制到数组开头，将position设置为最后一个没有读取的元素的位置
+        System.out.println(intBuffer.compact());
+
+        intBuffer.flip();
+        int[] des = new int[intBuffer.limit()];
+        intBuffer.get(des);
+        System.out.println(Arrays.toString(des));
+    }
 }
--- learning-java/src/main/java/training/yanghuadong/nio/FileChannelTest.java
+++ learning-java/src/main/java/training/yanghuadong/nio/FileChannelTest.java
@@ -1,8 +1,63 @@
 package training.yanghuadong.nio;
 
+import lombok.SneakyThrows;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * @author BirdSnail
  * @date 2019/12/5
  */
 public class FileChannelTest {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        RandomAccessFile file = new RandomAccessFile("learning-java/test.txt", "rw");
+        FileChannel channel = file.getChannel();
+        System.out.println(channel.size());
+//        readFile(channel);
+//        writeFile(channel);
+//        writeFileWithHole(channel, 20);
+    }
+
+    @SneakyThrows
+    private static void writeFile(FileChannel fileChannel) {
+        ByteBuffer byteWrite = ByteBuffer.allocate(48);
+        byteWrite.put("hello nio test".getBytes());
+
+        byteWrite.flip();
+        while (byteWrite.hasRemaining()) {
+            fileChannel.write(byteWrite);
+        }
+    }
+
+    @SneakyThrows
+    private static void writeFileWithHole(FileChannel fileChannel, long position) {
+        ByteBuffer byteWrite = ByteBuffer.allocate(48);
+        byteWrite.put("hello nio test".getBytes());
+
+        byteWrite.flip();
+        while (byteWrite.hasRemaining()) {
+            fileChannel.write(byteWrite, position);
+        }
+    }
+
+    @SneakyThrows
+    private static void readFile(FileChannel fileChannel) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+        int read = fileChannel.read(byteBuffer);
+        System.out.println("read bytes counts is " + read);
+
+        byteBuffer.flip();
+        byte[] words = new byte[byteBuffer.limit()];
+        byteBuffer.get(words);
+        System.out.println(new String(words));
+
+        while (byteBuffer.hasRemaining()) {
+            System.out.println((char) (byteBuffer.get()));
+        }
+    }
+
 }
