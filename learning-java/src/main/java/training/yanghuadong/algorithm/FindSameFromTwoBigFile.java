b0521014babe313cab6dabffd48814ef5ba392c0
yanghuadong <314726028@qq.com>
2020-07-30 23:01:44 +0800

    feat:完成分治算法的实现

--- learning-java/src/main/java/training/yanghuadong/algorithm/FindSameFromTwoBigFile.java
+++ learning-java/src/main/java/training/yanghuadong/algorithm/FindSameFromTwoBigFile.java
@@ -1,14 +1,9 @@
 package training.yanghuadong.algorithm;
 
-import com.google.common.hash.Hashing;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -23,21 +18,65 @@ import java.util.stream.Collectors;
  */
 public class FindSameFromTwoBigFile {
 
-	public static void main(String[] args) {
+    static int count = 10;
 
-	}
+    public static void main(String[] args) throws IOException {
+        Path fileA = Paths.get("E:\\ideaProject\\learning\\learning-java", "test.txt");
+        Path fileB = Paths.get("E:\\ideaProject\\learning\\learning-java", "b.txt");
 
-	public static void split(Path file) throws IOException {
-		List<String> allLines = Files.readAllLines(file);
-		Map<String, List<String>> listMap = allLines.stream()
-				.collect(Collectors.groupingBy(s -> s.hashCode() % 10 + ""));
+        Set<String> filesA = split(fileA, count);
+        Set<String> filesB = split(fileB, count);
+        filesB.retainAll(filesA);
 
-	}
+        for (String key : filesB) {
+            Path aPath = getSplitPath(fileA, key);
+            Path bPath = getSplitPath(fileB, key);
+            List<String> result = getSameLineFromPair(aPath, bPath);
+            if (!result.isEmpty()) {
+                System.out.println("存在相同的行：");
+                result.forEach(System.out::println);
+            }
+        }
+    }
 
-//	private static String hash(String line) {
-//		return Hashing.sha256()
-//				.hashString(line, StandardCharsets.UTF_8)
-//				.toString();
-//	}
+    /**
+     * 根据每行的hash值将改行写到不同的子文件中
+     *
+     * @param file  原始文件
+     * @param count 子文件个数
+     * @return 产生的子文件序号集合
+     * @throws IOException io异常
+     */
+    public static Set<String> split(Path file, int count) throws IOException {
+        List<String> allLines = Files.readAllLines(file);
+        Map<String, List<String>> lineMap = allLines.stream()
+                .collect(Collectors.groupingBy(s -> negativeHash(s) % count + ""));
+        for (String key : lineMap.keySet()) {
+            Path splitPath = getSplitPath(file, key);
+            Files.write(splitPath, lineMap.get(key), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        }
+        return lineMap.keySet();
+    }
+
+    public static Path getSplitPath(Path file, String key) {
+        String base = file.getFileName().toString();
+        String directory = file.getParent().getFileName().toString();
+        return Paths.get(directory, base + key);
+    }
+
+    public static List<String> getSameLineFromPair(Path one, Path two) throws IOException {
+        if (!Files.exists(one) || !Files.exists(two)) {
+            return Collections.emptyList();
+        }
+        List<String> linesOne = Files.readAllLines(one);
+        List<String> linesTwo = Files.readAllLines(two);
+        linesOne.retainAll(linesTwo);
+        return linesOne;
+    }
+
+    // 保证hash值一定为正数
+    private static int negativeHash(String s) {
+        return s.hashCode() & Integer.MAX_VALUE;
+    }
 
 }
